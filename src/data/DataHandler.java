1b164788ae541bb8304184a2c4dc7845c0efd09c
Jan Luellig <jluellig@mail.uni-mannheim.de>
2021-03-31 15:36:33 +0200

    #9 Basic Database Methods created, resource folder added

--- src/data/DataHandler.java
+++ src/data/DataHandler.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  * This class is used to handle all kinds of Data that has to be stored,
@@ -50,5 +55,212 @@ public class DataHandler {
 	 */
 	public static boolean checkWord(String string) {
 		return Dictionary.checkWord(string);
+	}
+	
+	/**
+	 * Creates a new player table in the PlayerDB with 
+	 * the given username and avatar.
+	 * 
+	 * @param username
+	 * @param avatar
+	 * @throws Exception
+	 *
+	 * @author jluellig
+	 */
+	public static void createPlayerInfoTable(String username, String avatar) throws Exception {
+		Connection con = null;
+		Statement stm = null;
+
+		try {
+			// connect to Database
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") + 
+					"resources" + System.getProperty("file.separator") + 
+					"PlayerDB.db");
+			
+			stm = con.createStatement();
+			
+			// Delete table PlayerInfo
+			stm.executeUpdate("DROP TABLE PlayerInfo");
+			
+			// Create new and empty table PlayerInfo
+			stm.executeUpdate("CREATE TABLE PlayerInfo (Username TEXT NOT NULL, Avatar TEXT NOT NULL)");
+			
+			// Add username and avatar to PlayerInfo
+			stm.executeUpdate("INSERT INTO PlayerInfo(Username, Avatar) VALUES('" + username + "', '"
+					+ avatar + "')");
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage() ); 
+			System.exit(0);
+		} finally {
+			if (con != null && stm != null) {
+				stm.close();
+				con.close();
+			}
+		}
+	}
+	
+	/**
+	 * Changes the username of the player in the database
+	 * 
+	 * @param username
+	 * @throws Exception
+	 *
+	 * @author jluellig
+	 */
+	public static void alterPlayerUsername(String username) throws Exception{
+		Connection con = null;
+		Statement stm = null;
+
+		try {
+			// connect to Database
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") + 
+					"resources" + System.getProperty("file.separator") + 
+					"PlayerDB.db");
+			
+			stm = con.createStatement();
+			
+			// Alter Username
+			stm.executeUpdate("UPDATE PlayerInfo SET Username = '" + username + "'");
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage() ); 
+			System.exit(0);
+		} finally {
+			if (con != null && stm != null) {
+				stm.close();
+				con.close();
+			}
+		}
+	}
+	
+	/**
+	 * Changes the avatar of the player in the database
+	 * 
+	 * @param avatar
+	 * @throws Exception
+	 *
+	 * @author jluellig
+	 */
+	public static void alterPlayerAvatar(String avatar) throws Exception{
+		Connection con = null;
+		Statement stm = null;
+
+		try {
+			// connect to Database
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") + 
+					"resources" + System.getProperty("file.separator") + 
+					"PlayerDB.db");
+			
+			stm = con.createStatement();
+			
+			// Alter Avatar
+			stm.executeUpdate("UPDATE PlayerInfo SET Avatar = '" + avatar + "'");
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage() ); 
+			System.exit(0);
+		} finally {
+			if (con != null && stm != null) {
+				stm.close();
+				con.close();
+			}
+		}
+	}
+	
+	/**
+	 * Returns the Usernames of the Players which are stored in the Database
+	 * 
+	 * @return Player Usernames
+	 * @throws Exception
+	 *
+	 * @author jluellig
+	 */
+	public static String[] getPlayerUsernames() throws Exception{
+		Connection con = null;
+		Statement stm = null;
+		ResultSet rs = null;
+		ArrayList<String> usernames = new ArrayList<String>(); 
+		
+		try {
+			// connect to Database
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") + 
+					"resources" + System.getProperty("file.separator") + 
+					"PlayerDB.db");
+			
+			stm = con.createStatement();
+			
+			// Get player Username
+			rs = stm.executeQuery("SELECT Username FROM PlayerInfo");
+			
+			while(rs.next()) {
+				usernames.add(rs.getString("Username"));
+			}
+			
+				
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage() ); 
+			System.exit(0);
+		} finally {
+			if (con != null && stm != null) {
+				if (rs != null) {
+					rs.close();
+				}
+				con.close();
+				stm.close();
+			}
+		}
+		return (String[]) usernames.toArray();
+	}
+	
+	/**
+	 * Returns the Avatar of the player which is stored in the database
+	 * 
+	 * @param username
+	 * @return avatar
+	 * @throws Exception
+	 *
+	 * @author jluellig
+	 */
+	public static String getPlayerAvatar(String username) throws Exception{
+		Connection con = null;
+		Statement stm = null;
+		ResultSet rs = null;
+		StringBuffer avatar = new StringBuffer();
+		
+		try {
+			// connect to Database
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + System.getProperty("file.separator") + 
+					"resources" + System.getProperty("file.separator") + 
+					"PlayerDB.db");
+			
+			stm = con.createStatement();
+			
+			// Get player Avatar
+			rs = stm.executeQuery("SELECT Avatar FROM PlayerInfo WHERE (Username = '" + username + "')");
+			
+			while(rs.next()) {
+				avatar.append(rs.getString("Avatar"));
+			}
+			
+				
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage() ); 
+			System.exit(0);
+		} finally {
+			if (con != null && stm != null) {
+				if (rs != null) {
+					rs.close();
+				}
+				con.close();
+				stm.close();
+			}
+		}
+		return avatar.toString();
 	}
 }
