ed1c46a12f7b1072624e48d15166c9176eea9da8
Luca Sebastian Steltmann <lsteltma@mail.uni-mannheim.de>
2021-04-18 15:54:25 +0200

    #24 formatted for Google Java Style

--- src/network/messages/DisconnectMessage.java
+++ src/network/messages/DisconnectMessage.java
@@ -1,28 +1,28 @@
 package network.messages;
 
 /**
- * Implementation of the disconnect message. A DisconnectMessage is created for
- * every Player leaving a game session
+ * Implementation of the disconnect message. A DisconnectMessage is created for every Player leaving
+ * a game session
  *
  * @author lsteltma
  */
 public class DisconnectMessage extends Message {
 
-	/**
-	 * Default serialization UID
-	 *
-	 * @author lsteltma
-	 */
-	private static final long serialVersionUID = 1L;
+  /**
+   * Default serialization UID
+   *
+   * @author lsteltma
+   */
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Constructor: Creates a DisconnectMessage by using the MessageType.DISCONNECT
-	 * and the String name of the person disconnecting
-	 *
-	 * @author lsteltma
-	 * @param name
-	 */
-	public DisconnectMessage(String name) {
-		super(MessageType.DISCONNECT, name);
-	}
+  /**
+   * Constructor: Creates a DisconnectMessage by using the MessageType.DISCONNECT and the String
+   * name of the person disconnecting
+   *
+   * @author lsteltma
+   * @param name
+   */
+  public DisconnectMessage(String name) {
+    super(MessageType.DISCONNECT, name);
+  }
 }
