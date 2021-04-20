70af7d1592141615f253d282e3b0d0cf4476c33d
Jan Oliver Bleil <jbleil@mail.uni-mannheim.de>
2021-04-20 15:01:37 +0200

    #10 added comments to the Main class

--- src/screens/Main.java
+++ src/screens/Main.java
@@ -3,7 +3,20 @@ package screens;
 
 import javafx.application.Application;
 
+/**
+ * This class launches the UI
+ * 
+ * @author jbleil
+ */
+
 public class Main{
+	
+	/**
+	 * this method launches the Application
+	 * 
+	 * @author jbleil
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		Application.launch(StartScreen.class);
