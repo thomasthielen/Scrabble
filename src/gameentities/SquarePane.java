24cfae639877e095522c15e6ba319d40b6468f35
Luca Sebastian Steltmann <lsteltma@mail.uni-mannheim.de>
2021-05-05 15:05:30 +0200

    #10 created SquarePane class

---
+++ src/gameentities/SquarePane.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package gameentities;
+
+import javafx.scene.layout.StackPane;
+
+/**
+ * @author lsteltma
+ *
+ */
+public class SquarePane {
+	private Square square;
+	private StackPane stackPane;
+	
+	public SquarePane(Square square) {
+		this.square = square;
+		this.stackPane = new StackPane();
+	}
+	
+	public Square getSquare() {
+		return this.square;
+	}
+	
+	public StackPane getStackPane() {
+		return this.stackPane;
+	}
+}
