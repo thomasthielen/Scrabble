c07158ac0da5b03d08c6f7119078c5cb7feaabb6
Luca Sebastian Steltmann <lsteltma@mail.uni-mannheim.de>
2021-05-06 16:52:00 +0200

    #10 added comments

--- src/gameentities/SquarePane.java
+++ src/gameentities/SquarePane.java
@@ -1,13 +1,25 @@
-/** */
+/** 
+ * Implementation of the SquarePane objects
+ * 
+ * <p>The attributes include a Square and a StackPane
+ * 
+ * @author lsteltma 
+ * */
 package gameentities;
 
 import javafx.scene.layout.StackPane;
 
-/** @author lsteltma */
 public class SquarePane {
   private Square square;
   private StackPane stackPane;
 
+/**
+ * 
+ * Constructor: Creates a SquarePane object by adding a given square to a new StackPane
+ *
+ * @author lsteltma
+ * @param square
+ */
   public SquarePane(Square square) {
     this.square = square;
     this.stackPane = new StackPane();
