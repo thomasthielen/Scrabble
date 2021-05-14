fd5c9a548caf0c45245572685ac8d0349dac9658
Jan Luellig <jluellig@mail.uni-mannheim.de>
2021-05-14 15:53:22 +0200

    #32 error messages & check if username is already used added

--- src/screens/NewProfileScreenController.java
+++ src/screens/NewProfileScreenController.java
@@ -3,12 +3,16 @@ package screens;
 import data.DataHandler;
 import gameentities.Avatar;
 import gameentities.Player;
+
+import java.util.HashMap;
 import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 /**
@@ -31,7 +35,17 @@ public class NewProfileScreenController {
   @FXML
   void startGame(ActionEvent event) throws Exception {
     // TODO add avatar listener
-    if (Pattern.matches("[a-zA-Z0-9]{2,15}", inputForm.getText().trim())) {
+    // check if the input matches an already created username
+    boolean isAlreadyUsed = false;
+    HashMap<Integer, String[]> profiles = DataHandler.getPlayerInfo();
+    for (int key : profiles.keySet()) {
+      String s = (String) profiles.get(key)[0];
+      if (s.equals(inputForm.getText().trim())) {
+    	  isAlreadyUsed = true;
+    	  break;
+      }
+    }
+    if (Pattern.matches("[a-zA-Z0-9]{2,15}", inputForm.getText().trim()) & !isAlreadyUsed) {
       String playerName = inputForm.getText().trim();
       Avatar a = Avatar.BLUE;
       DataHandler.addPlayer(playerName, a);
@@ -42,13 +56,25 @@ public class NewProfileScreenController {
       Parent content = loader.load();
       StartScreen.getStage().setScene(new Scene(content));
       StartScreen.getStage().show();
+    } else if (isAlreadyUsed) {
+      Alert errorAlert = new Alert(AlertType.ERROR);
+      errorAlert.setHeaderText("Username already exists.");
+      errorAlert.setContentText("Try a different username.");
+      errorAlert.showAndWait();
+      inputForm.clear();
     } else {
-      // TODO
+      Alert errorAlert = new Alert(AlertType.ERROR);
+      errorAlert.setHeaderText("Input not valid");
+      errorAlert.setContentText(
+          "The username must contain 2-15 letters or numbers. It can't contain any special characters.");
+      errorAlert.showAndWait();
+      inputForm.clear();
     }
   }
 
   /**
-   * This method serves as the Listener for "Back"-Button It redirects the user to the Start Screen
+   * This method serves as the Listener for "Back"-Button. It redirects the user to the Start
+   * Screen.
    *
    * @author jbleil
    * @param event
