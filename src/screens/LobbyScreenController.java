43a9e0c6a2222d047e84e399e25201cfed6e7cd2
Thomas Andre Thielen <tthielen@mail.uni-mannheim.de>
2021-05-17 20:28:55 +0200

    #35 "fix" for the display of the rack of the host

--- src/screens/LobbyScreenController.java
+++ src/screens/LobbyScreenController.java
@@ -94,7 +94,7 @@ public class LobbyScreenController {
       Client.getGameSession().getPlayer().setCurrentlyPlaying(true); 
       Client.reportStartGame(DataHandler.getOwnPlayer());
       Client.getGameSession().initialiseGameScreen();
-      switchToGameScreen();
+      //switchToGameScreen(); TODO: Leads to exception, but doesn't break game. No idea why.
     }
   }
 
