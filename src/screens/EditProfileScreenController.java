5f0484f15a40fb56dd699ce474da1fbc875efe1a
Jan Luellig <jluellig@mail.uni-mannheim.de>
2021-05-18 13:27:38 +0200

    #32 back button fixed

--- src/screens/EditProfileScreenController.java
+++ src/screens/EditProfileScreenController.java
@@ -97,6 +97,8 @@ public class EditProfileScreenController {
     FXMLLoader loader = new FXMLLoader();
     loader.setLocation(getClass().getResource("resources/ExistingProfileScreen.fxml"));
     Parent content = loader.load();
+    ExistingProfileScreenController existingProfileScreenController = loader.getController();
+    existingProfileScreenController.addProfiles();
     StartScreen.getStage().setScene(new Scene(content));
     StartScreen.getStage().show();
   }
