754c7fb4b8c2037f37ee2f0b8dda1f02bf002821
Jan Oliver Bleil <jbleil@mail.uni-mannheim.de>
2021-05-21 00:40:52 +0200

    #51 implemented first draft of EndScreen

---
+++ src/screens/EndScreenController.java
@@ -0,0 +1,13 @@
+package screens;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EndScreenController {
+  
+  @FXML
+  void leaveGame(ActionEvent event) throws Exception {}
+
+  @FXML
+  void playAgain(ActionEvent event) throws Exception {}
+}
