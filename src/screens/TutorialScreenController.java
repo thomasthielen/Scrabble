249bab2c0e9f48dfc469b3f76350008109fb8d2d
Jan Oliver Bleil <jbleil@mail.uni-mannheim.de>
2021-05-25 20:23:11 +0200

    #16 added TutorialScreen

---
+++ src/screens/TutorialScreenController.java
@@ -0,0 +1,26 @@
+package screens;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class TutorialScreenController {
+  
+  @FXML
+  private Pane backgroundPane;
+
+  @FXML
+  void previous(ActionEvent event) throws Exception {
+
+  }
+
+  @FXML
+  void next(ActionEvent event) throws Exception {
+
+  }
+
+  @FXML
+  void leaveTutorial(ActionEvent event) throws Exception {
+
+  }
+}
