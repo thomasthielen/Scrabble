0e90616f09177c5c2d1bcb3d25661f1026de075c
Luca Sebastian Steltmann <lsteltma@mail.uni-mannheim.de>
2021-05-27 12:22:44 +0200

    #63 test class for UC1

--- test/data/ManagePlayerProfilesTest.java
+++ test/data/ManagePlayerProfilesTest.java
@@ -2,12 +2,53 @@ package data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import gameentities.Avatar;
+
+/**
+ * This is the test class to make sure the functional requirements of the UC1: Manage Player
+ * Profiles work correctly
+ *
+ * @author lsteltma
+ */
 class ManagePlayerProfilesTest {
 
+  private String[] data;
+
+  // create player profiles
   @Test
-  void test() {
-    fail("Not yet implemented");
+  @Order(1)
+  void testCreateProfile() {
+    DataHandler.addPlayer("name", Avatar.BLACK);
+
+    data = DataHandler.getPlayerInfo().get(DataHandler.getOwnPlayerId());
+
+    assertEquals("name", data[0]);
+    assertEquals("BLACK", data[1]);
+  }
+
+  // read and update player profiles
+  @Test
+  @Order(2)
+  void testUpdatePlayerProfile() {
+
+    DataHandler.alterPlayerUsername("alteredName", DataHandler.getOwnPlayerId());
+    DataHandler.alterPlayerAvatar(Avatar.RED, DataHandler.getOwnPlayerId());
+
+    data = DataHandler.getPlayerInfo().get(DataHandler.getOwnPlayerId());
+
+    assertEquals("alteredName", data[0]);
+    assertEquals("RED", data[1]);
+  }
+
+  // delete player profiles
+  @Test
+  @Order(3)
+  void testDeletePlayerProfile() {
+    DataHandler.deletePlayer(DataHandler.getOwnPlayerId());
+
+    assertNull(DataHandler.getPlayerInfo().get(DataHandler.getOwnPlayerId()));
   }
 }
