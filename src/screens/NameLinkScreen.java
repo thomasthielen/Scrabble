a76c00ef1698d876b3a03e521728a287360af967
Jan Oliver Bleil <jbleil@mail.uni-mannheim.de>
2021-05-29 14:26:22 +0200

    #27 added comments

--- src/screens/NameLinkScreen.java
+++ src/screens/NameLinkScreen.java
@@ -1,28 +1,46 @@
 package screens;
 
 import javafx.application.Application;
+
+/**
+ * This class provides the methods to launch the Screen for the Name Link of "Soni Sokell" which is
+ * present on every screen that uses avatars.
+ *
+ * @author jbleil
+ */
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-public class NameLinkScreen extends Application { 
-  
+public class NameLinkScreen extends Application {
+
+  /**
+   * this method launches the application.
+   *
+   * @author jbleil
+   * @param args String[] of the main method
+   */
   public static void main(String[] args) {
     launch(args);
   }
-  
+
+  /**
+   * sets the scene of the stage.
+   *
+   * @author jbleil
+   */
   public void start(Stage primaryStage) {
     WebView webView = new WebView();
-    
+
     webView.getEngine().load("https://freeicons.io/profile/6200");
-    
+
     VBox vBox = new VBox(webView);
-    
+
     Scene scene = new Scene(vBox, 1000, 562);
-    
+
     primaryStage.setScene(scene);
-    
+
     primaryStage.show();
   }
 }
--- src/screens/SiteLinkScreen.java
+++ src/screens/SiteLinkScreen.java
@@ -6,11 +6,29 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+/**
+ * This class provides the methods to launch the Screen for the Site Link of "freeicons.io" which is
+ * present on every screen that uses avatars.
+ *
+ * @author jbleil
+ */
 public class SiteLinkScreen extends Application {
+
+  /**
+   * this method launches the application.
+   *
+   * @author jbleil
+   * @param args String[] of the main method
+   */
   public static void main(String[] args) {
     launch(args);
   }
 
+  /**
+   * sets the scene of the stage.
+   *
+   * @author jbleil
+   */
   public void start(Stage primaryStage) {
     WebView webView = new WebView();
 
